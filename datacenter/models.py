657f06c66878dd9855061b750f53db2c9033328a
Dmitriy Klepikov <dmitriy@Dmitriys-MacBook-Pro.local>
2020-09-06 22:43:34 +0300

    initial

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import django
 
 
 class Passcard(models.Model):
@@ -23,5 +24,38 @@ class Visit(models.Model):
         return "{user} entered at {entered} {leaved}".format(
             user=self.passcard.owner_name,
             entered=self.entered_at,
-            leaved= "leaved at " + str(self.leaved_at) if self.leaved_at else "not leaved"
+            leaved="leaved at " + str(self.leaved_at) if self.leaved_at else "not leaved"
         )
+
+
+def get_duration(visit):
+    if visit.leaved_at is None:
+        spent_time = django.utils.timezone.localtime() - visit.entered_at
+        return spent_time
+    else:
+        return visit.leaved_at - visit.entered_at
+
+
+def format_duration(duration):
+    seconds = duration.total_seconds()
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    spent_time_string = f'{hours}ч {minutes}мин'
+    return spent_time_string
+
+
+def is_visit_long(visit, minutes=60):
+    duration_seconds = get_duration(visit).seconds
+    duration_minutes = (duration_seconds % 3600) // 60
+    if visit.leaved_at is None and duration_minutes > minutes:
+        return True
+    elif visit.leaved_at is None and duration_minutes < minutes:
+        return False
+    stay_seconds = (visit.leaved_at - visit.entered_at).seconds
+    stay_minutes = (stay_seconds % 3600) // 60
+    if visit.leaved_at is not None and stay_minutes > minutes:
+        return True
+    elif visit.leaved_at is not None and stay_minutes < minutes:
+        return False
+    else:
+        return False
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,23 @@
 from datacenter.models import Passcard
-from datacenter.models import Visit
+from datacenter.models import Visit, get_duration, format_duration
 from django.shortcuts import render
+import django
 
 
 def storage_information_view(request):
     # Программируем здесь
-
-    non_closed_visits = [
-        {
-            "who_entered": "Richard Shaw",
-            "entered_at": "11-04-2018 25:34",
-            "duration": "25:03",
+    active_visits = Visit.objects.filter(leaved_at=None)
+    not_closed_visits = []
+    for visit in active_visits:
+        name = visit.passcard.owner_name
+        enter_time = django.utils.timezone.localtime(visit.entered_at)
+        non_closed_visits = {
+            "who_entered": name,
+            "entered_at": enter_time,
+            "duration": format_duration(get_duration(visit)),
         }
-    ]
+        not_closed_visits.append(non_closed_visits)
     context = {
-        "non_closed_visits": non_closed_visits,  # не закрытые посещения
+        "non_closed_visits": not_closed_visits,
     }
     return render(request, 'storage_information.html', context)
