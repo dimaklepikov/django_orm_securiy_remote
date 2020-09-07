1d7f2d6bbcb5ca05ea3985533a4e224396025ce5
Dmitriy Klepikov <dmitriy@Dmitriys-MacBook-Pro.local>
2020-09-07 13:58:40 +0300

    passcard duration beautify

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'datacenter/templates')],
         'APP_DIRS': True,
     },
 ]
