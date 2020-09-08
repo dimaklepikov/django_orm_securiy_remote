cc8188479965df39414809e3d90ed95bdb02deb6
Dmitriy Klepikov <dmitriy@Dmitriys-MacBook-Pro.local>
2020-09-08 19:48:24 +0300

    fixed redame

--- project/settings.py
+++ project/settings.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from environs import Env
-load_dotenv(dotenv_path=' .env', verbose=True)
+load_dotenv(dotenv_path='.env', verbose=True)
 env = Env()
 env.read_env()
 DATABASES = {
